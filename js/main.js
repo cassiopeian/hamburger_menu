81f3f796a4182e77ce3dc8af7f78a033f8ab5a14
cassiopeia <cassiopeia.neely@gmail.com>
2019-10-03 19:20:38 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,17 +4,17 @@
 
 // 1. on clicking of the hamburger, show the nav by animating it's left value to 0
 $('.hamburger').on('click', function(){
-  $('header nav').animate({left: 0});
+  $('nav').animate({left: 0});
 });
 
 // 2. on clicking of the close button in the nav, hide the nav by animating it's left value back to 100%
 $('.close-nav').on('click', function(){
-  $('header nav').animate({left: '100%'});
+  $('nav').animate({left: '100%'});
 });
 
 // 3. on resizing of the browser, we want to reset any styles set if we are back on desktop). Look into removeAttr to remove the inline styles that's being added to the nav element
 // $(window).on("resize", function(){
 $(window).on('resize', function(){
-  // $('header nav').removeAttr('style');
+  $('nav').removeAttr('style');
 });
 
